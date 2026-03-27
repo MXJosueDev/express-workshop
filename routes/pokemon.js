88386b09381f289e1044e694a6768289c586cab4
Josue Lopez <mxjosuelr@gmail.com>
2026-03-27 02:20:59 -0600

    Sesion 7

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -2,13 +2,9 @@ const express = require("express");
 const pokemon = express.Router();
 const db = require("../config/database");
 
-pokemon.post("/", (req, res, next) => {
-  return res.status(200).send(req.body);
-});
-
 pokemon.get("/", async (req, res, next) => {
   const pk = await db.query("SELECT * FROM pokemon;");
-  return res.status(200).json(pk);
+  return res.status(200).json({code: 1, message: pk});
 });
 
 // Aqui le pasé directamente una expresion regular, porque en express v5 reservaron algunos caracteres en los paths
@@ -19,10 +15,10 @@ pokemon.get(/^\/([0-9]{1,3})$/, async (req, res, next) => {
   const id = req.params[0] - 1; // el offset
   console.log(pk.length);
   if (!(id >= 0 && id <= pk.length - 1)) {
-    return res.status(404).send("Pokemon no encontrado");
+    return res.status(404).json({code: 404, message: "Pokemon no encontrado"});
   }
 
-  return res.status(200).send(pk[id]);
+  return res.status(200).send({code: 1, message: pk[id]});
 });
 
 pokemon.get(/^\/([A-Za-z]+)$/, async (req, res, next) => {
@@ -34,8 +30,8 @@ pokemon.get(/^\/([A-Za-z]+)$/, async (req, res, next) => {
   const pkmn = pk.filter(pkmn => pkmn.pok_name.toUpperCase() == name.toUpperCase() && pkmn);
 
   return pkmn.length == 0
-    ? res.status(404).send("Pokemon no encontrado")
-    : res.status(200).send(pkmn);
+    ? res.status(404).json({code: 404, message: "Pokemon no encontrado"})
+    : res.status(200).send({code: 1, message: pk[id]});
 });
 
 module.exports = pokemon;
