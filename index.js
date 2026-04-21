29404dc1b43edfbde878508ff52c1ed4ac630d37
Josue Lopez <mxjosuelr@gmail.com>
2026-04-21 17:51:57 -0600

    Act 12. Add middleware for authentication and response handling

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require("express");
 const app = express();
 const pokemon = require("./routes/pokemon");
 const user = require("./routes/user");
+const auth = require("./middleware/auth");
+const notFound = require("./middleware/notFound");
+const index = require("./middleware/index");
 
 /*
 Métodos HTTP
@@ -18,16 +21,15 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res, next) => {
-  return res.status(200).json({ code: 200, message: "Bienvenido al Pokedex" });
-});
+app.get("/", index);
 
-app.use("/pokemon", pokemon);
 app.use("/user", user);
 
-app.use((req, res, next) => {
-  return res.status(404).json({ code: 404, message: "URL no encontrada" });
-});
+app.use(auth);
+
+app.use("/pokemon", pokemon);
+
+app.use(notFound);
 
 app.listen(process.env.port || 3000, () => {
   console.log("Server is running...");
