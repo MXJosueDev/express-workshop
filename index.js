fbf6fdab26fa1225afc7cde6bd703aeb07b836a4
Josue Lopez <mxjosuelr@gmail.com>
2026-04-21 18:19:46 -0600

    Act 13

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const user = require("./routes/user");
 const auth = require("./middleware/auth");
 const notFound = require("./middleware/notFound");
 const index = require("./middleware/index");
+const cors = require("./middleware/cors");
 
 /*
 Métodos HTTP
@@ -20,6 +21,8 @@ DELETE
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors);
 
 app.get("/", index);
 
